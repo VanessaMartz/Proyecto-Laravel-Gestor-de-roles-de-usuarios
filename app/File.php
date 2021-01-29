b071b1a1455528d0e6f1cfbdedf6b35d79b5486d
Vanessa Martz <vane.martz@hotmail.com>
2021-01-29 15:36:45 -0600

    Create File.php

---
+++ app/File.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Traits\SortableTrait;
+
+class File extends Model
+{
+    //librería para comportamiento ordenable a un modelo
+    use SortableTrait;
+
+    protected $fillable = [
+        'title', 'description', 'path'
+    ];
+
+	public function scopeSearch($q)
+    {
+        return empty(request()->search) ? $q : $q->where('title', 'like', '%'.request()->search.'%')->orWhere('description', 'like', '%'.request()->search.'%');
+    }
+}
