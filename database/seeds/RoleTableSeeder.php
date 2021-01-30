a851f3fb78ad55afa624fbf59147330c894e4e85
Vanessa Martz <vane.martz@hotmail.com>
2021-01-30 00:37:05 -0600

    Update RoleTableSeeder.php

--- database/seeds/RoleTableSeeder.php
+++ database/seeds/RoleTableSeeder.php
@@ -12,7 +12,7 @@ class RoleTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        
         $role_employee = new Role();
 	    $role_employee->name = 'admin';
 	    $role_employee->description = 'Administrador';
