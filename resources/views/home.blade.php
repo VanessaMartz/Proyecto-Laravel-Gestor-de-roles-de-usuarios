fe76852c56cc88839aac83ac2db26f5e5081a91d
Vanessa Martz <vane.martz@hotmail.com>
2021-01-29 19:40:13 -0600

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    Estas logueado!
+                   ¡ Estas logueado!
                 </div>
             </div>
         </div>
